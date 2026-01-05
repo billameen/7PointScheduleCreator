320ec1a4e0b7f146e750234f1c5e7b1709183222
billameen <bilal.ameen@icloud.com>
2026-01-04 23:36:13 -0500

    Properly waits for Book tab to fully load

--- src/main.py
+++ src/main.py
@@ -7,19 +7,31 @@ dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
 
 with sync_playwright() as p:
-    # login
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
+
+    # login
     page.goto("https://www.7pointops.com/login")
     page.locator("#userName").fill(os.getenv("username"))
     page.locator("#password").fill(os.getenv("password"))
     page.click("#loginButton")
+    page.wait_for_url("https://www.7pointops.com")
 
     # navigate to Book
-    page.wait_for_url("https://www.7pointops.com")
     page.goto("https://www.7pointops.com/book")
-    print("waiting")
     page.wait_for_url("https://www.7pointops.com/book")
-    page.wait_for_timeout(10000)
+    page.locator("#locationColumnWrapper").wait_for(timeout=30000)
+    print("done")
+
+    # wait for book to load
+    # get all the rooms that could be in use today
+    # get all the events that a room has
+
+    # create a class for each time slot
+    #
+
+    # id=locationColumnWrapper -> contains all room numbers
+    # class=buildingsContainer -> contains all events
+
     browser.close()
 
