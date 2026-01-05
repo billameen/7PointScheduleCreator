3a627ea0dd575fac3ba3f5b1e7145e8d4b7f0487
billameen <bilal.ameen@icloud.com>
2026-01-05 16:29:16 -0500

    Partially implemented scraping event info

--- src/main.py
+++ src/main.py
@@ -1,37 +1,71 @@
 import os
+from dataclasses import dataclass
+from typing import Optional
+
 from dotenv import load_dotenv, find_dotenv
 from playwright.sync_api import sync_playwright
 
-# load username and password
-dotenv_path = find_dotenv()
-load_dotenv(dotenv_path)
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
+# This dataclass will store all information pertaining to an event
+@dataclass
+class Event:
+    name: Optional[str] = None
+    room: Optional[str] = None
+    start_time: Optional[str] = None
+    end_time: Optional[str] = None
+    access_time: Optional[str] = None
+    setup_desc: Optional[str] = None
+    error: Optional[bool] = None
 
-    # login
-    page.goto("https://www.7pointops.com/login")
-    page.locator("#userName").fill(os.getenv("username"))
-    page.locator("#password").fill(os.getenv("password"))
-    page.click("#loginButton")
-    page.wait_for_url("https://www.7pointops.com")
 
-    # navigate to Book
-    page.goto("https://www.7pointops.com/book")
-    page.wait_for_url("https://www.7pointops.com/book")
-    page.locator("#locationColumnWrapper").wait_for(timeout=30000)
-    print("done")
 
-    # wait for book to load
-    # get all the rooms that could be in use today
-    # get all the events that a room has
+def main():
+    # load username and password
+    dotenv_path = find_dotenv()
+    load_dotenv(dotenv_path)
 
-    # create a class for each time slot
-    #
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
 
-    # id=locationColumnWrapper -> contains all room numbers
-    # class=buildingsContainer -> contains all events
+        # login
+        page.goto("https://www.7pointops.com/login")
+        page.locator("#userName").fill(os.getenv("username"))
+        page.locator("#password").fill(os.getenv("password"))
+        page.click("#loginButton")
+        page.wait_for_url("https://www.7pointops.com")
 
-    browser.close()
+        # navigate to Book
+        page.goto("https://www.7pointops.com/book")
+        page.wait_for_url("https://www.7pointops.com/book")
+        page.locator("#locationColumnWrapper").wait_for(timeout=30000)
 
+        # get all rooms
+        # reservedRooms = {}
+        # for roomName in page.locator("#locationColumnWrapper").all_inner_texts():
+        #     reservedRooms[roomName] = []
+
+        for event in page.locator(".eventBarText").all():
+            event.click()
+
+            page.locator("#ngplus-overlay-container").wait_for(state="hidden")
+            # access the info inside the event
+            roomnum = page.locator(".roomDesc").inner_text()
+            headinfo = page.locator("h3").inner_text()
+            # timeinfo = page.locator(".groupDetails>dl").first.inner_html()
+            # accesstime = page.get_by_text("Event Timeline").inner_text(timeout=1000)
+
+            print("roomnum:", roomnum)
+            print("headinfo:", headinfo)
+            # print("timeInfo:", timeinfo)
+            # print("accesstime:", accesstime)
+
+            page.wait_for_timeout(3000)
+
+            # exit this event
+            page.get_by_role("button").get_by_text("×").click()
+
+        browser.close()
+
+
+main()
